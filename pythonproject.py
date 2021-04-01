94b0e27acef3e31c371eb0eb66d6ece0a52da2b7
gerardine <gerardinekaba@gmail.com>
2021-04-01 08:29:32 +0300

    pythonproject

---
+++ pythonproject.py
@@ -0,0 +1,67 @@
+# if __name__ == '__main__':
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#  #    print('primary'.name)
+# #     print('primary'.age)
+# #     print('primary'.country)
+# #
+# #
+# #
+# #
+# #
+# # class School:
+# #     def speaking(self):
+# #         print("My school is "+self.name)
+# #         primary:School=School()
+# #         name:str= "Gerardine"
+# #         age:int=20
+# #         country:str="Rwanda"
+if __name__ == '__main__':
+    student=20
+    print(student)
+    # Arthimetic Operation
+    a=23
+    b=12
+    print(a+b)
+    q=90
+    w=5
+    print(q%w)
+    # Comparion operator
+    a=98
+    c=87
+    print(a!=c)
+    x=2
+    x+=10
+    print(x)
+    # String Escaping
+    greet="Hello students"
+    print("Hello\b students")
+    print("Hello \n students")
+    print("Hello\v students")
+
+
+
+# String Formating
+person={'name':'Gerardine','school':'AkiraChix',}
+print('Hello my name is' +person['name']+ 'i am currently a student at'+person['school'])
+
+# List Comprehension
+x=[10,20,30,40,50]
+modulus=[v%3 for v in x]
+print(modulus)
+
+
+
